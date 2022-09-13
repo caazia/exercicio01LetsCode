21e0cb79876370e6561534ff1482d3eddbd286b0
Cássia Carvalho <110124751+caazia@users.noreply.github.com>
2022-09-13 01:52:19 -0300

    Add files via upload

---
+++ um.java
@@ -0,0 +1,15 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class um {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Entre com um número de 1 a 10");
+        int numero = input.nextInt();
+        System.out.println("A tabuada de " + numero + " é: ");
+        for(int i=1; i <= 10; i++) {
+            System.out.println(numero + " x " + i + " = " + i*numero);
+        }
+    }
+}
